d192a876049b93d54e2c9dd3bcdcc307c1b48d67
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-07-06 21:05:14 -0300

    add type definitions

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+export type Theme = {
+    h1?: string;
+    h2?: string;
+    h3?: string;
+    h4?: string;
+    h5?: string;
+    h6?: string;
+    a?: string;
+    figcaption?: string;
+    blockquote?: string;
+    code?: string;
+    inlineCode?: string;
+    codeNumbers?: string;
+    dt?: string;
+    del?: string;
+    ins?: string;
+    italic?: string;
+    strike?: string;
+    underline?: string;
+    bold?: string;
+    samp?: string;
+    kbd?: string;
+    var?: string;
+    mark?: string;
+};
+
+export default function (rawHtml: string, theme: Theme = {}): string;
